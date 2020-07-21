ab23bccf75517368c6d4df97f8f40bd77b1beb76
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2020-07-21 20:53:53 -0300

    First commit

---
+++ CSharpNewVersion/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CSharpNewVersion
+{
+    interface IPerson
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        DateTime BirthDate { get; set; }
+
+        int GetAge()
+        {
+            return DateTime.Now.Year - BirthDate.Year;
+        }
+    }
+
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+
+    class User : IPerson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+
+
+    class Class1
+    {
+    }
+}
---
+++ CSharpOldVersion/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CSharpOldVersion
+{
+    public class Class1
+    {
+    }
+}
