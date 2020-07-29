9664a64a87b83e64c374d26f8fbdf5755256ad63
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2020-07-29 20:32:44 -0300

    Testing disposable ref structs;

---
+++ CSharpNewVersion/DisposableRefStructs.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpNewVersion
+{
+    ref struct MyStructWithReference
+    {
+        public void Dispose()
+        {
+            DisposableRefStructs.IsMyStructWithReferenceDead = true;
+        }
+    }
+
+    class DisposableRefStructs
+    {
+        public static bool IsMyStructWithReferenceDead { get; set; } = false;
+
+        [Test]
+        public void DisposableRefStructsTest()
+        {
+            using (var myStructWithReferente = new MyStructWithReference())
+            {
+
+            } // is disposed
+
+            Assert.That(IsMyStructWithReferenceDead, Is.True);
+        }
+    }
+}
---
+++ CSharpOldVersion/DisposableRefStructs.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharpNewVersion
+{
+    internal struct MyStructWithReference
+    {
+        public void Dispose()
+        {
+            DisposableRefStructs.IsMyStructWithReferenceDead = true;
+        }
+    }
+
+    internal class DisposableRefStructs
+    {
+        public static bool IsMyStructWithReferenceDead { get; set; } = false;
+
+        public void DisposableRefStructsTest()
+        {
+            //using (var myStructWithReferente = new MyStructWithReference())
+            //{
+            //}
+            // can't implement using
+        }
+    }
+}
