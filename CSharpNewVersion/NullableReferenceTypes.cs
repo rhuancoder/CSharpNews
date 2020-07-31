fd3c749b23d22938befd19d1f7119c1b93bc68fa
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2020-07-30 21:35:20 -0300

    Testing nullable reference types;

---
+++ CSharpNewVersion/NullableReferenceTypes.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpNewVersion
+{
+    class MyClass { }
+
+
+    class NullableReferenceTypes
+    {
+        [Test]
+        public void NullableReferenceTypesTest()
+        {
+            string name = null;
+
+            MyClass? myClass = null;
+        }
+
+        [Test]
+        public void NullableReferenceTypesWithNullableWarnTest()
+        {
+#nullable enable
+            string name = null;
+
+            MyClass myClass = null;
+#nullable restore
+        }
+    }
+}
---
+++ CSharpOldVersion/NullableReferenceTypes.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharpNewVersion
+{
+    class MyClass { }
+
+
+    class NullableReferenceTypes
+    {
+        public void NullableReferenceTypesTest()
+        {
+            string name = null;
+
+            MyClass myClass = null;
+        }
+
+        public void NullableReferenceTypesWithNullableWarnTest()
+        {
+            string name = null;
+
+            MyClass myClass = null;
+        }
+    }
+}
