51b341ae164d37459e59c76dbf468fb4b3c7cf64
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2020-07-30 21:56:38 -0300

    Testing async streams;

---
+++ CSharpOldVersion/AsyncStreams.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpNewVersion
+{
+    class AsyncStreams
+    {
+        //public async IAsyncEnumerable<int> GetNumbers()
+        //{
+        //    for(int i = 1; i <= 20; i++)
+        //    {
+        //        await Task.Delay(100);
+        //        yield return i;
+        //    }
+        //}
+
+        //public async Task AsyncStreamsTest()
+        //{
+        //    await foreach(var number in GetNumbers())
+        //    {
+        //        Debug.WriteLine(number);
+        //    }
+        //}
+    }
+}
