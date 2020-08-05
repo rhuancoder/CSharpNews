3cc9f8943ba0a9ee7d2d3b0bb022998c5494d08c
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2020-08-05 20:26:04 -0300

    Testing indexes and ranges;

---
+++ CSharpNewVersion/IndexesAndRanges.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace CSharpNewVersion
+{
+    class IndexesAndRanges
+    {
+        [Test]
+        public void IndexesAndRangesTest()
+        {
+            var words = new string[]
+            {
+                "cachorro",
+                "gato",
+                "passarinho",
+                "elefante"
+            };
+
+            Assert.That(words[0], Is.EqualTo("cachorro"));
+
+            Assert.That(words[^1], Is.EqualTo("elefante"));
+
+            Assert.That(words[words.Length -1], Is.EqualTo("elefante"));
+
+            foreach(var word in words[2..^0])
+            {
+                Debug.WriteLine(word);
+            }
+
+            Range range = 1..^0;
+
+            foreach (var word in words[range])
+            {
+                Debug.WriteLine(word);
+            }
+
+            Assert.That(words[words.Length - 1], Is.EqualTo("elefante"));
+        }
+    }
+}
---
+++ CSharpOldVersion/IndexesAndRanges.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace CSharpNewVersion
+{
+    internal class IndexesAndRanges
+    {
+        public void IndexesAndRangesTest()
+        {
+            //var words = new string[]
+            //{
+            //    "cachorro",
+            //    "gato",
+            //    "passarinho",
+            //    "elefante"
+            //};
+
+            //foreach (var word in words[2..^0])
+            //{
+            //    Debug.WriteLine(word);
+            //}
+
+            //Range range = 1..^0;
+
+            //foreach (var word in words[range])
+            //{
+            //    Debug.WriteLine(word);
+            //}
+        }
+    }
+}
