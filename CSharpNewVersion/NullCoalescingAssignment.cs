b94c99c44eea1400be14544e695e88f0f490ffbd
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2020-08-10 21:38:25 -0300

    Testing Null-coalescing assignment;

---
+++ CSharpNewVersion/NullCoalescingAssignment.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpNewVersion
+{
+    class NullCoalescingAssignment
+    {
+        [Test]
+        public void NullCoalescingAssigmentFirstTest()
+        {
+            List<int> numbers = null;
+
+            if(numbers == null)
+            {
+                numbers = new List<int>() { 1, 2, 3 };
+            }
+
+            Assert.That(numbers, Is.Not.Null);
+        }
+
+        [Test]
+        public void NullCoalescingAssigmentSecondTest()
+        {
+            List<int> numbers = null;
+
+            numbers = numbers ?? new List<int>() { 1, 2, 3 };
+
+            Assert.That(numbers, Is.Not.Null);
+        }
+
+        [Test]
+        public void NullCoalescingAssigmentThirdTest()
+        {
+            List<int> numbers = null;
+
+            numbers ??= new List<int>() { 1, 2, 3 };
+
+            Assert.That(numbers, Is.Not.Null);
+        }
+    }
+}
---
+++ CSharpOldVersion/NullCoalescingAssignment.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpNewVersion
+{
+    internal class NullCoalescingAssignment
+    {
+        public void NullCoalescingAssigmentFirstTest()
+        {
+            List<int> numbers = null;
+
+            if (numbers == null)
+            {
+                numbers = new List<int>() { 1, 2, 3 };
+            }
+        }
+
+        public void NullCoalescingAssigmentSecondTest()
+        {
+            List<int> numbers = null;
+
+            numbers = numbers ?? new List<int>() { 1, 2, 3 };
+        }
+
+        //public void NullCoalescingAssigmentThirdTest()
+        //{
+        //    List<int> numbers = null;
+
+        //    numbers ??= new List<int>() { 1, 2, 3 };
+        //}
+    }
+}
