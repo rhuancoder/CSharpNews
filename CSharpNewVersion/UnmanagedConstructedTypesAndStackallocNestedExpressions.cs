4459cea0e89187dfc8a5746f9775e4b07768836c
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2020-08-12 20:54:52 -0300

    Testing Unmanaged constructed types and Stackalloc  in nested expressions;

---
+++ CSharpNewVersion/UnmanagedConstructedTypesAndStackallocNestedExpressions.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpNewVersion
+{
+    public struct Coords<T>
+    {
+        public T X;
+        public T Y;
+    }
+
+    class UnmanagedConstructedTypesAndStackallocNestedExpressions
+    {
+        [Test]
+        public void UnmanagedConstructedTypesAndStackallocNestedExpressionsTest()
+        {
+            Span<Coords<int>> coordinates = stackalloc[]
+            {
+                new Coords<int> { X = 0, Y = 2 },
+                new Coords<int> { X = 50, Y = 30 }
+            };
+
+            Span<int> numbers = stackalloc[] { 1, 2, 3, 4 };
+            var ind = numbers.IndexOfAny(stackalloc[] { 3, 4 });
+
+            Assert.That(ind, Is.EqualTo(2));
+        }
+    }
+}
---
+++ CSharpOldVersion/UnmanagedConstructedTypesAndStackallocNestedExpressions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharpNewVersion
+{
+    public struct Coords<T>
+    {
+        public T X;
+        public T Y;
+    }
+
+    class UnmanagedConstructedTypesAndStackallocNestedExpressions
+    {
+        public void UnmanagedConstructedTypesAndStackallocNestedExpressionsTest()
+        {
+            //Span<Coords<int>> coordinates = stackalloc[]
+            //{
+            //    new Coords<int> { X = 0, Y = 2 },
+            //    new Coords<int> { X = 50, Y = 30 }
+            //};
+
+            //Span<int> numbers = stackalloc[] { 1, 2, 3, 4 };
+            //var ind = numbers.IndexOfAny(stackalloc[] { 3, 4 });
+        }
+    }
+}
