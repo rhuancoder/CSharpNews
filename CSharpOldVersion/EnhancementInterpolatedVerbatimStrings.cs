7a317d2c9b18c13030cd75b6c84d626ed5d72ba1
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2020-08-13 21:23:46 -0300

    Testing enhancement of interpolated verbatim strings;

---
+++ CSharpOldVersion/EnhancementInterpolatedVerbatimStrings.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharpNewVersion
+{
+    internal class EnhancementInterpolatedVerbatimStrings
+    {
+        public void EnhancementInterpolatedVerbatimStringsTest()
+        {
+            var price = 10;
+
+            var message = "O valor do item é de " + price.ToString() + "!" +
+                " Boas compras!";
+
+            var message2 = $"O valor do item é de {price.ToString()}! " +
+                " Boas compras!";
+
+            var message3 = $@"O valor do item é de {price.ToString()}!
+                  Boas compras!";
+
+            //var message4 = @$"O valor do item é de {price.ToString()}!
+            //      Boas compras!";
+        }
+    }
+}
