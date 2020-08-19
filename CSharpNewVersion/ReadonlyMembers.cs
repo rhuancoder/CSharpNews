1ea6a84df42d43c4a6eaa83a78163ce743ec9fdb
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2020-08-18 22:34:45 -0300

    Testing readonly members;

---
+++ CSharpNewVersion/ReadonlyMembers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpNewVersion
+{
+    public struct Point
+    {
+        public readonly decimal X { get; }
+        public readonly decimal Y { get; }
+    }
+
+    class ReadonlyMembers
+    {
+    }
+}
