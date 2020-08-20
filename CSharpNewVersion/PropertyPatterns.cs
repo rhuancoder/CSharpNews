6e9944425e10d5b51377a1a3ad4b19dc8534f681
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2020-08-19 22:12:12 -0300

    Testing property patterns;

---
+++ CSharpNewVersion/PropertyPatterns.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpNewVersion
+{
+    class Address
+    {
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+
+    class PropertyPatterns
+    {
+        public decimal GetTax(Address address) =>
+            address switch
+            {
+                { State: "RJ"} => 10,
+                { State: "PR"} => 13,
+                _ => 5
+            };
+
+        [Test]
+        public void PropertyPatternsTest()
+        {
+            var firstAddress = new Address() { State = "RJ" };
+            var secondAddress = new Address() { State = "PR" };
+            var thirdAddress = new Address() { State = "MG" };
+
+            var firstTax = GetTax(firstAddress);
+            var secondTax = GetTax(secondAddress);
+            var thirdTax = GetTax(thirdAddress);
+
+            Assert.That(firstTax, Is.EqualTo(10));
+            Assert.That(secondTax, Is.EqualTo(13));
+            Assert.That(thirdTax, Is.EqualTo(5));
+        }
+    }
+}
---
+++ CSharpOldVersion/PropertyPatterns.cs
@@ -0,0 +1,29 @@
+﻿namespace CSharpNewVersion
+{
+    class Address
+    {
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+
+    class PropertyPatterns
+    {
+        //public decimal GetTax(Address address) =>
+        //    address switch
+        //    {
+        //        { State: "RJ"} => 10,
+        //        { State: "PR"} => 13,
+        //        _ => 5
+        //    };
+        //public void PropertyPatternsTest()
+        //{
+        //    var firstAddress = new Address() { State = "RJ" };
+        //    var secondAddress = new Address() { State = "PR" };
+        //    var thirdAddress = new Address() { State = "MG" };
+
+        //    var firstTax = GetTax(firstAddress);
+        //    var secondTax = GetTax(secondAddress);
+        //    var thirdTax = GetTax(thirdAddress);
+        //}
+    }
+}
