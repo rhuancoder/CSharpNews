e8e3f7e638ea1c36ce1ee1ff6b90ea81a44eac09
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2020-08-21 21:01:17 -0300

    Testing positional patterns;

---
+++ CSharpOldVersion/PositionalPatterns.cs
@@ -0,0 +1,44 @@
+﻿namespace CSharpNewVersion
+{
+    class NewPoint
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public NewPoint(int x, int y) => (X, Y) = (x, y);
+        public void Deconstruct(out int x, out int y) =>
+            (x, y) = (X, Y);
+    }
+
+    enum Quadrant
+    {
+        Unknown,
+        Origin,
+        One,
+        Two,
+        Three,
+        Four,
+        OnBorder
+    }
+
+    //class PositionalPatterns
+    //{
+    //    Quadrant GetQuadrant(NewPoint point) =>
+    //        point switch
+    //        {
+    //            (0, 0) => Quadrant.Origin,
+    //            var (x, y) when x > 0 && y > 0 => Quadrant.One,
+    //            var (x, y) when x < 0 && y > 0 => Quadrant.Two,
+    //            var (x, y) when x < 0 && y < 0 => Quadrant.Three,
+    //            var (x, y) when x > 0 && y < 0 => Quadrant.Four,
+    //            var (_, _) => Quadrant.OnBorder,
+    //            _ => Quadrant.Unknown
+    //        };
+
+    //    public void PositionalPatternsTest()
+    //    {
+    //        var point = new NewPoint(1, 2);
+
+    //        var quadrant = GetQuadrant(point);
+    //    }
+    //}
+}
